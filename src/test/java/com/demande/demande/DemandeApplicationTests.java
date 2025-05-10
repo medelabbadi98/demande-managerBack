48db5b1b13910ea515f805b911532bfd0b1e2c87
medelabbadi98 <med.el.abbadi98@gmail.com>
2025-05-10 01:20:58 +0100

    back-end setup

---
+++ src/test/java/com/demande/demande/DemandeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demande.demande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+class DemandeApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+    }
+}
+
+
